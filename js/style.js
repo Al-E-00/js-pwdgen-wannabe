7f9f838f0d9d8e4ef0fbf15abf631d87b54e52dc
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-17 16:13:59 +0200

    js work!!

--- js/style.js
+++ js/style.js
@@ -2,4 +2,4 @@ const nomeUtente = prompt("Qual è il tuo nome?");
 const cognomeUtente = prompt("Qual è il tuo cognome?");
 const colorePreferito = prompt("Qual è il tuo colore preferito?");
 
-console.log(nomeUtente + cognomeUtente + colorePreferito + "22");
+passwordGenerator.innerHTML = (nomeUtente + cognomeUtente + colorePreferito + "22");
